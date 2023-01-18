493e079e1cebba3173ee336cb592043925bc623d
TomasMagnin <tomasmagnin@hotmail.com>
2023-01-18 01:18:52 -0300

    FS_2

--- Desafio_N2.js
+++ Desafio_N2.js
@@ -6,15 +6,14 @@ class ProductManager {
     
     constructor(path) {
         this.path = path;
-        /* this.products = [] */
+        //this.products = []
         this.id = 1
     }
 
-    async addProduct(path)
 
-    /* async addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct(title, description, price, thumbnail, code, stock) {
 
-        const finderCode = this.products.find(item => item.code == code);
+        //const finderCode = this.products.find(item => item.code == code);
 
         if(title && description && price && thumbnail && code && stock) {
             let producto = {
@@ -24,62 +23,84 @@ class ProductManager {
                 price,
                 thumbnail,
                 code,
-
                 stock
-            }
-            if (finderCode) {
+            } 
+           /*  if(finderCode) {
                 console.error('Code already exists');
                 return;
-            }
+            } */
             
-            this.products.push(title && description && price && thumbnail && code && stock)
-            await fs.promises.writeFile(this.producto, JSON.stringify(title && description && price && thumbnail && code && stock))
+            const file = await this.getProducts();
+            file.push(producto);
+            await this.writeFile(this.path, file);
+            console.log(`Producto agregado con exito ${this.id}`);
 
 
-
-            console.log('Producto agregado con exito');
+            
+             /* const aa = this.products.push(producto)                     // Ver mas adelante
+            console.log(aa);
+            const pdtExt = JSON.stringify(aa)
+            console.log(pdtExt);
+            await fs.promises.writeFile(this.path, pdtExt) */
         }else {
             console.log('Validar Campos');
         }
 
-    } */
-
-    async getProducts() {
-        let productsG = await fs.promises.readFile(this.path, 'utf-8')
-        let objProducts = JSON.parse(productsG)
-        console.log(objProducts); 
     }
 
+    async getProducts() {
+        let productsG = await fs.promises.readFile(this.path, 'utf-8');
+        let objProducts = JSON.parse(productsG);
+        //console.log(objProducts); 
+        return objProducts
+    }
 
     getProductsById(id) {
-        let find = this.products.find(item => item.id == id)
+        let find = this.products.find(item => item.id == id);
         return find ? this.products[id-1] : "Not found";
     }
 
+    async delteProduct(id){
+        //await fs.promises.writeFile(this.path, '[]');
+        const file = await this.getProducts();
+        //console.log(file);
+        const fileFilter = file.filter(item => item.id !== id) || null;     // Creamos un array con todos los procudctos que no coincidan con el id
+        //console.log(fileFilter);
+        await this.writeFile(this.path, fileFilter);
 
-   
+
+    }  
     
+    async writeFile(path, dataWrite){
+        const writeData = JSON.stringify(dataWrite);
+        await fs.promises.writeFile(this.path, writeData);
+    }
+
+    async updateProduct(id, data){
+        const file = await this.getProducts();
+        const findID = file.find(item => item.id == id);
+        console.log(findID);
+        if(findID){
+            const aux = file.findIndex(item => item.id == id)
+            data.id = id;
+            file.splice(aux, 1, data);
+            await this.writeFile(this.path, data)
+        }
+
+
+    }
 }
 
 
-
-    let poductManager = new ProductManager('products.json')        // Creamos la clase product manager                
+    let poductManager = new ProductManager('./products.json')        // Creamos la clase product manager                
 
     const main = async () => {
-        let product1 = {
-                title: 'Venzo',
-                description: 'Bike',
-                price: 2000,
-                thumbnail: 'asdasd',
-                code: 'asdsa',
-                stock: 5
-        }
+    
+    // poductManager.addProduct('Titile1', 'Primer Producto', 5, 'http.img', 456, 40);
+    //poductManager.addProduct('Titile2', 'Segundo Producto', '10', 'http.img', 457, 60);
+    //poductManager.getProducts(); 
+   // poductManager.delteProduct(1);
+    poductManager.updateProduct(1,{"id": 1, "title": "Titile2", "description": "Segundo_Producto", "price": 10, "thumbnail": "http.img", code: 457, "stock": 60});
     }
-    /* poductManager.addProduct('Titile1', 'Primer Producto', 5, 'http.img', 456, 40);
-    poductManager.addProduct('Titile2', 'Segundo Producto', '10', 'http.img', 457, 60);
-    poductManager.addProduct('Titile3', 'Segundo Producto', '10', 'http.img', 458, 60); */
-    //await poductManager.addProduct(product1);
-    //await poductManager.addProduct('Titile2', 'Segundo Producto', '10', 'http.img', 457, 60);
-    poductManager.getProducts(); 
-    //const finderID = poductManager.getProductsById(2);
-    //console.log(finderID);
+
+    main()
